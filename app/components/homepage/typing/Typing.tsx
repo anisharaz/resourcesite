b63883b78aae97e242957a8a7d3418fa4c046f7d
Thinley <thinleylama44@gmail.com>
2024-03-03 21:31:56 +0530

    Made typing component

--- app/components/homepage/typing/Typing.tsx
+++ app/components/homepage/typing/Typing.tsx
@@ -1,5 +1,12 @@
+import Image from "next/image";
+import giphy from "../assets/giphy.gif"
+
 function Typing() {
-  return <div className="text-tert">Typing Animation</div>;
+  return (
+    <div className="flex justify-center mt-10">
+        <Image src={giphy} width={300} alt="image" />
+    </div>
+  )
 }
 
 export default Typing;
