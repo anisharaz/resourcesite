75980f0e4ab086719e8f9f3d7dabbafc4a72ff98
Thinley <thinleylama44@gmail.com>
2024-03-06 22:33:29 +0530

    pdf added successfully

---
+++ app/components/Pdf/BodyPdf.tsx
@@ -0,0 +1,10 @@
+import PDF from "./PDF"
+
+export default function BodyPdf() {
+    return (
+        <div>
+            {/* pdf render statically  */}
+            <PDF />
+        </div>
+    )
+}
--- app/modules_view/page.tsx
+++ app/modules_view/page.tsx
@@ -1,3 +1,5 @@
+import BodyPdf from "../components/Pdf/BodyPdf";
+
 export default function modules_view({
   searchParams,
 }: {
@@ -5,9 +7,12 @@ export default function modules_view({
 }) {
   return (
     <div>
-      {searchParams.module_path +
-        " " +
-        JSON.parse(searchParams.other_modules)[2]}
+      <BodyPdf />
     </div>
+    // <div>
+    //   {searchParams.module_path +
+    //     " " +
+    //     JSON.parse(searchParams.other_modules)[2]}
+    // </div>
   );
 }
