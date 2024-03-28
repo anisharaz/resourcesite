abc40b57cba7df82460f8c2465a5b234a09a3229
Thinley <thinleylama44@gmail.com>
2024-03-28 20:52:56 +0530

    Server side rendering and npm run build successful without any errors.

--- app/components/landing/navbar/navbar.tsx
+++ app/components/landing/navbar/navbar.tsx
@@ -1,78 +1,28 @@
-'use client'
-
 import * as React from "react"
-import { useMediaQuery } from '@react-hook/media-query'
+
 import { Button } from "@/components/ui/button"
-import { MenuIcon } from "lucide-react"
 import logo from "../assets/logo.png"
 import Image from "next/image";
 import Link from "next/link"
-import {
-  Drawer,
-  DrawerClose,
-  DrawerContent,
-  DrawerFooter,
-  DrawerHeader,
-  DrawerTitle,
-  DrawerTrigger,
-} from "@/components/ui/drawer"
-import { useState, useEffect } from 'react'
+import MobileNav from "../mobileNav/mobileNav"
 
-export default function Navbar() {
-  const [isClient, setIsClient] = useState(false)
-  const [open, setOpen] = React.useState(false)
-  const isDesktop = useMediaQuery("(min-width: 768px)")
+const Navbar = function() {
 
-  useEffect(() => {
-    setIsClient(true)
-  }, [])
-
-  // If client is not yet available, don't render anything
-  if (!isClient) {
-    return null;
-  }
-
-  // Render navbar once client is available
   return (
-    <div className="flex justify-between bg-[#301E67] p-3">
-      <div className="ml-2 mt-2">
-        <Image src={logo} alt="" className="logo h-7 w-auto rounded-full" />
+      <div className="flex justify-between bg-[#301E67] p-2">
+        <div className="ml-2 mt-2">
+          <Image src={logo} alt="" className="logo h-7 w-auto rounded-full" />
+        </div>
+        <div className="hidden md:flex">
+          <Button className="mr-4 text-xl text-white" variant="link"><Link href={"/"}>Home</Link></Button>
+          <Button className="mr-4 text-xl text-white" variant="link"><Link href={"/admin"}>Admin</Link></Button>
+          <Button className="mr-4 text-xl text-white" variant="link"><Link href={"/"}>About</Link></Button>
+          <Button className="mr-4 text-xl text-white" variant="link"><Link href={"/"}>Contact</Link></Button>
+        </div>
+        <div className="md:hidden">
+          <MobileNav />
+        </div>
       </div>
-      <div>
-        {isDesktop ? (
-          <>
-            <Button className="mr-4 text-xl text-white" variant="link"><Link href={"/"}>Home</Link></Button>
-            <Button className="mr-4 text-xl text-white" variant="link"><Link href={"/admin"}>Admin</Link></Button>
-            <Button className="mr-4 text-xl text-white" variant="link"><Link href={"/"}>About</Link></Button>
-            <Button className="mr-4 text-xl text-white" variant="link"><Link href={"/"}>Contact</Link></Button>
-          </>
-        ) : (
-          <Drawer open={open} onOpenChange={setOpen}>
-            <DrawerTrigger asChild>
-              <div className="flex justify-end">
-                <Button style={{ background: "#301E67" }}>
-                  {/* Burger menu icon */}
-                  <MenuIcon className="text-white h-5 w-5" />
-                </Button>
-              </div>
-            </DrawerTrigger>
-            <DrawerContent style={{ background: "#070041" }} className="text-white">
-              <DrawerHeader className="text-center">
-                <DrawerTitle>Choose an option</DrawerTitle>
-              </DrawerHeader>
-              <Button variant="link"><Link href={"/"} className="text-white">Home</Link></Button>
-              <Button variant="link"><Link href={"/admin"} className="text-white">Admin</Link></Button>
-              <Button variant="link"><Link href={"/"} className="text-white">About</Link></Button>
-              <Button variant="link"><Link href={"/"} className="text-white">Contact</Link></Button>
-              <DrawerFooter className="pt-2">
-                <DrawerClose asChild>
-                  <Button style={{ background: "#070041" }} variant="outline">Cancel</Button>
-                </DrawerClose>
-              </DrawerFooter>
-            </DrawerContent>
-          </Drawer>
-        )}
-      </div>
-    </div>
   )
 }
+export default Navbar;
