4974e32c9a03e0e6cc0a47a614fdf6dd97f90e52
Anish Araz <anisharaz919@gmail.com>
2024-06-10 10:43:16 +0530

    UPDATE: UI and colors

--- app/components/HomePage/components/Hero.tsx
+++ app/components/HomePage/components/Hero.tsx
@@ -8,9 +8,12 @@ export default async function Hero() {
         src={bgImage}
         alt="image"
       />
-      <div className="HeroText absolute text-3xl md:text-5xl lg:text-6xl top-12 md:top-1/3 left-7 md:left-20 font-extrabold flex flex-col gap-2 md:text-center">
-        <div>One Place For All Notes & resources</div>
-        <div className="text-base md:text-2xl lg:text-3xl">
+      <div className="absolute text-3xl md:text-5xl lg:text-6xl top-12 md:top-1/3 left-7 md:left-20 font-extrabold flex flex-col gap-2 md:text-center">
+        <div>
+          One Place For{" "}
+          <span className="stroke text-transparent">All Notes</span> & resources
+        </div>
+        <div className="text-xl md:text-2xl lg:text-4xl">
           Scroll And Start Exploring Course wise
         </div>
       </div>
