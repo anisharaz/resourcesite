a3ee5cbcb9338cfdd531ed79e7e4383aacd69a44
manish-lal12 <manishlal2274@gmail.com>
2024-11-25 21:41:59 +0530

    Updated Module View

--- app/(ReadNotes)/view/[branch]/[sem]/[subject_code]/page.tsx
+++ app/(ReadNotes)/view/[branch]/[sem]/[subject_code]/page.tsx
@@ -1,6 +1,7 @@
 import { TileSkeletons } from "@/components/ui/Skeletons";
 import ModuleList from "./ModuleList";
 import { Suspense } from "react";
+// import ModuleNavbar from "@/components/ui/ModuleNavbar";
 
 async function Modules({
   params,
@@ -8,14 +9,17 @@ async function Modules({
   params: Promise<{ branch: string; sem: string; subject_code: string }>;
 }) {
   const param = await params;
+  // const subjectCode = (await params).subject_code;
+
   return (
     <>
       <div
-        className="bg-lpri dark:bg-dpri p-2"
+        className="bg-gray-100 p-2"
         style={{
           minHeight: "calc(100vh - 52px)",
         }}
       >
+        {/* <ModuleNavbar subject_code={subjectCode} /> */}
         <Suspense fallback={<TileSkeletons />}>
           <ModuleList
             branch={param.branch}
